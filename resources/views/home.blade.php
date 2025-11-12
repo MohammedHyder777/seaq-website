7c47abb73d57e8ab40889750c643aff0febf73d8
mohammedhyder777 <mohammedhyder@hotmail.com>
2025-11-12 13:03:23 +0300

    fix home cards length in md screens

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,11 +13,15 @@
 <div class="mutual-list space-y-6">
   @foreach ($posts as $post)
   <div class="md:flex bg-white md:h-100 mutual-list-card rounded-xl shadow-md overflow-hidden hover:shadow-xl transition mh-card opacity-0 translate-y-10 transition-all duration-700 ease-out">
-    <img src="{{ isset($post->image)? asset('storage/'.$post->image) : asset('images/logos/logo-w-text.png') }}" class="w-full md:w-1/2 md:h-full object-cover" alt="خبر">
-    <div class="flex flex-col justify-between p-7 pb-15 w-full">
+    {{-- Image --}}
+    <img src="{{ isset($post->image)? asset('storage/'.$post->image) : asset('images/logos/logo-w-text.png') }}" 
+      class="w-full h-auto md:w-1/2 md:h-full object-cover" alt="خبر">
+    
+    {{-- Text --}}
+    <div class="flex flex-col justify-between p-7 pb-15 w-full md:w-1/2">
       <div class="flex flex-col">
         <h3 class="text-lg text-justify font-semibold mb-2">{{ $lang == 'ar'? $post->title : $post->title_en }}</h3>
-        <p class="text-gray-600 text-justify">&nbsp;&nbsp;&nbsp;{{ Str::limit($lang == 'ar'? $post->body : $post->body_en, 350) }}</p>
+        <p class="text-gray-600 text-justify">&nbsp;&nbsp;&nbsp;{{ Str::limit($lang == 'ar'? $post->body : $post->body_en, 250) }}</p>
       </div>
       <a href="{{ route('posts.show', $post) }}" class="text-left text-teal-700 hover:text-teal-800 cursor-pointer font-semibold">
         اقرأ المزيد ←
