5caa4d394a569eb0898c995b6e84298a8fbd6b60
mohammedhyder777 <mohammedhyder@hotmail.com>
2025-12-20 21:44:07 +0300

    Add animation to hammburger button

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -15,9 +15,9 @@
         <a class="flex items-center px-3 text-xl font-medium {{ Request::is('join') ? 'text-sky-600 bg-slate-100' : 'text-neutral-100 hover:text-sky-600 hover:bg-slate-100' }}" href="{{ route('join') }}">{{__('strings.join_us')}}</a>
         <a class="flex items-center px-3 text-xl font-medium {{ Request::is('career') ? 'text-sky-600 bg-slate-100' : 'text-neutral-100 hover:text-sky-600 hover:bg-slate-100' }}" href="{{ route('career') }}">{{__('strings.career_development')}}</a>
         <!-- <a class="py-3 px-3 text-xl font-medium {{ Request::is('about') ? 'text-sky-600 bg-slate-100' : 'text-neutral-100 hover:text-sky-600 hover:bg-slate-100' }}" href="{{ route('about') }}">{{__('strings.about')}}</a> -->
-        
+
         <!-- Dropdown wrapper -->
-        
+
         <div class="relative flex items-center" data-dropdown>
             <!-- Toggle button -->
             <button
@@ -60,11 +60,18 @@
     <!-- Mobile title and toggle button -->
     <div class="md:hidden flex flex-col space-y-5 backdrop-blur-md">
         <div class="flex items-center justify-between px-4 py-3 bg-teal-600/90 backdrop-blur-sm shadow-lg text-white">
-            <button id="toggle-btn" class="p-4" onclick="document.getElementById('mobile-nav').classList.toggle('hidden')">
+            <!-- <button id="toggle-btn" class="p-4" onclick="document.getElementById('mobile-nav').classList.toggle('hidden')">
                 <svg class="h-6 w-6 text-neutral-100" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
+            </button> -->
+            
+            <button class="hamburger" id="toggle-btn" class="p-4" onclick="document.getElementById('mobile-nav').classList.toggle('hidden'); this.classList.toggle('open');">
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
             </button>
+
             <!-- Logo for wide screens -->
             <a class="block md:w-min p-1 bg-white rounded-4xl text-left" href="{{ url('/') }}">
                 <img src="{{ asset('images/logos/logo.png') }}" height="20" width="86" alt="الشعار">
@@ -110,6 +117,7 @@
             // Check if the click was outside both the mobileNav and toggle button
             if (!mobileNav.contains(event.target) && !toggle.contains(event.target)) {
                 mobileNav.classList.add('hidden');
+                toggle.classList.toggle('open'); // Change the button shape to hamburger
             }
         });
     </script>
